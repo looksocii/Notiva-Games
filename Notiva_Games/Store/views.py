8efa0c5ebe83d815f35f3cacd967203c91192bef
Puntakarn Kaewsanghan <aopup1122@gmail.com>
2022-04-03 22:10:57 +0700

    set img upload and create index page

--- Notiva_Games/Store/views.py
+++ Notiva_Games/Store/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from Store.models import *
 
-# Create your views here.
+def index(request):
+    games = Games.objects.all()
+    context = {
+        'Games': games
+    }
+    return render(request, template_name='Store/index.html', context=context)
+
+def game(request, id):
+    game = Games.objects.get(pk=id)
+    context = {
+        'Game': game
+    }
+    return render(request, template_name='Store/game.html', context=context)
